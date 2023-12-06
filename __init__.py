3f82c46225140d54aa59c2bbfbe353514c3c25d0
oksy <852496245@qq.com>
2023-12-06 10:08:08 +0800

    新增强制绘制动态的gm工具

---
+++ __init__.py
@@ -0,0 +1,1 @@
+from .get_dynamic_pic import get_dynamic_pic  # noqa: F401
---
+++ get_dynamic_pic.py
@@ -0,0 +1,42 @@
+from typing import Annotated
+from nonebot.rule import to_me, Namespace, ArgumentParser
+from nonebot.params import ShellCommandArgs
+from protocol_adapter.adapter_type import AdapterMessageEvent
+from protocol_adapter.protocol_adapter import ProtocolAdapter
+from nonebot import on_shell_command
+from plugins.common_plugins_function import white_list_handle
+from kmrbot.painter.dynamic_painter.dynamic_painter import DynamicPainter
+from utils.permission import only_me
+
+parser = ArgumentParser()
+parser.add_argument("--dynamic_id", "--id")
+get_dynamic_pic = on_shell_command(
+    "get_dynamic",
+    aliases={"dynamic_get"},
+    rule=to_me(),
+    parser=parser,
+    priority=5)
+
+get_dynamic_pic.handle()(white_list_handle("gm_cmd"))
+get_dynamic_pic.handle()(only_me)
+
+
+@get_dynamic_pic.handle()
+async def _(
+    event: AdapterMessageEvent,
+    params: Annotated[Namespace, ShellCommandArgs()]
+):
+    msg = ProtocolAdapter.MS.reply(event)
+    params = vars(params)
+    dynamic_id = params.get("dynamic_id") or params.get("id")
+    if dynamic_id is None or not dynamic_id.isdecimal():
+        msg += ProtocolAdapter.MS.text("Invalid Dynamic_id!")
+        await get_dynamic_pic.finish(msg)
+    dynamic_id = int(dynamic_id)
+    image = await DynamicPainter.generate_dynamic_pic(dynamic_id)
+    if image is None:
+        msg += ProtocolAdapter.MS.text(f"Dynamic_id {dynamic_id} is not exist!")
+        await get_dynamic_pic.finish(msg)
+    msg += ProtocolAdapter.MS.text(f"Dynamic_id {dynamic_id}\n")
+    msg += ProtocolAdapter.MS.image(image)
+    await get_dynamic_pic.finish(msg)
