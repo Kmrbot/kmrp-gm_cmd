c3a4028d5100e4adf3fc4b4f4c8f68cff9583185
oksy <852496245@qq.com>
2024-04-01 01:34:03 +0800

    修复绘制动态报错问题

--- get_dynamic_pic.py
+++ get_dynamic_pic.py
@@ -1,4 +1,5 @@
 from typing import Annotated
+from nonebot.log import logger
 from nonebot.rule import to_me, Namespace, ArgumentParser
 from nonebot.params import ShellCommandArgs
 from protocol_adapter.adapter_type import AdapterMessageEvent
@@ -7,6 +8,7 @@ from nonebot import on_shell_command
 from utils.permission import white_list_handle
 from kmrbot.painter.dynamic_painter.dynamic_painter import DynamicPainter
 from utils.permission import only_me
+from utils.bili.http_request import BiliHttpRequest
 
 
 parser = ArgumentParser()
@@ -34,7 +36,13 @@ async def _(
         msg += ProtocolAdapter.MS.text("Invalid Dynamic_id!")
         await get_dynamic_pic.finish(msg)
     dynamic_id = int(dynamic_id)
-    image = await DynamicPainter.generate_dynamic_pic(dynamic_id)
+    url = f"https://api.bilibili.com/x/polymer/web-dynamic/v1/detail?timezone_offset=-480&id={dynamic_id}"
+    dynamic_data = await BiliHttpRequest.get(url)
+    if dynamic_data is None or dynamic_data.get("item") is None:
+        logger.error("dynamic data is None !")
+        await get_dynamic_pic.finish()
+    dynamic_data = dynamic_data["item"]
+    image = await DynamicPainter.generate_dynamic_pic(dynamic_data)
     if image is None:
         msg += ProtocolAdapter.MS.text(f"Dynamic_id {dynamic_id} is not exist!")
         await get_dynamic_pic.finish(msg)
